7eefde80453bee5c24f7145e7883adedd69e3896
nozellamila <nozellamila@gmail.com>
2019-08-05 20:53:39 -0300

    Interrupt TimerOne não funciona em I2C

--- Pos_trapezio_interrupt/Pos_trapezio_interrupt.ino
+++ Pos_trapezio_interrupt/Pos_trapezio_interrupt.ino
@@ -1,5 +1,9 @@
 // Biblioteca para comunicação I2C
 #include <Wire.h>
+//Biblioteca para interrupt do arduino
+#include<TimerOne.h>
+
+#define TIMER_US 10100 //Tempo entre interrupções: 1,1ms
 
 // Endereço I2C do acelerometro
 const int MPU_addr=0x68; 
@@ -12,6 +16,8 @@ float aceleracoes[100];
 float velocidades[100];
 // Variavel para armazenar quantas amostras ja foram pegas
 char contagem=0;
+// Variavel para armazenar o valor medido
+int AcX; 
 
 void setup(){
   velocidades[0] = 0;
@@ -21,52 +27,52 @@ void setup(){
   Wire.write(0); // Manda 0 e "acorda" o MPU 6050
   Wire.endTransmission(true);
 
+  Timer1.initialize(TIMER_US);                  // Initialise timer1
+  Timer1.attachInterrupt( lerAcc );           // attach the ISR routine here
+  
   Serial.begin(19200); //Inicia a comunicaçao serial (para exibir os valores lidos)
 }
 
 void loop(){
-  // Variavel para armazenar o valor medido
-  int AcX; 
 
-  // Fica lendo o sensor até o valor em x ser maior que 800
-  while (abs(AcX) < 800){
-    // Mede o tempo total no inicio da medição e mais a frente subtrai para saber o tempo percorrido
-    tempo = micros();
-    
-    Wire.beginTransmission(MPU_addr); // Começa a transmissao de dados para o sensor
-    Wire.write(0x3B); // Registrador dos dados em x (ACCEL_XOUT_H)
-    Wire.endTransmission();
-    Wire.requestFrom(MPU_addr,2); // Faz um "pedido" para ler 2 registradores
-
-    // Se estiverem dados disponíveis para leitura
-    if(Wire.available()<=2) { 
-      AcX=Wire.read()<<8; // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-      AcX=AcX|Wire.read();  
-    }
-    
-  }
 
   AcX = abs(AcX);
   // Converte o valor medido
   aceleracoes[contagem] = (((float)AcX - (-32768)) * (2 - (-2)) / (32768 - (-32768)) + (-2))*9.81;
   // Calcula o tempo percorrido
-  tempo = micros()-tempo;
+  //tempo = micros()-tempo;
   
   //Se já fez 100 mediçoes, faz o calculo do deslocamento
   if(contagem == 99){
     contagem=0;
     float posicao;
-    posicao += calculoTrapezio(tempo)*100; // A função retorna o valor em metros, então multiplico por 100 para converter para centímetros
+    //posicao += calculoTrapezio(tempo)*100; // A função retorna o valor em metros, então multiplico por 100 para converter para centímetros
     
-    Serial.print("\nMoveu ");
-    Serial.print(posicao,6);
-    Serial.println(" cms em x.");
+    //Serial.print("\nMoveu ");
+    //Serial.print(posicao,6);
+    //Serial.println(" cms em x.");
     
   }else{
     contagem++;
   }
-
   
+}
+
+//rotina de interrupção
+void lerAcc(){
+  /*
+  Wire.beginTransmission(MPU_addr); // Começa a transmissao de dados para o sensor
+  Wire.write(0x3B); // Registrador dos dados em x (ACCEL_XOUT_H)
+  Wire.endTransmission();
+  Wire.requestFrom(MPU_addr,2); // Faz um "pedido" para ler 2 registradores
+
+  // Se estiverem dados disponíveis para leitura
+  if(Wire.available()<=2) { 
+    AcX=Wire.read()<<8; // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+    AcX=AcX|Wire.read();  
+  }
+  */
+  Serial.println("interrupt");
 }
 
 float calculoTrapezio(unsigned long tempo){
